4138efab82c89171954bd272c9148b1b507c20b2
Gauti Nils Bernhardsson <gauti@reontech.com>
2019-07-08 17:57:23 +0000

    Hero section and event container

--- assets/js/event-carousel.js
+++ assets/js/event-carousel.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-    var events = new Swiper ('.swiper-container.event-carousel', {
-      direction: 'horizontal',
-      loop: true,
-      slidesPerView: 3,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.swiper-button.next',
-        prevEl: '.swiper-button.prev',
-      },
-    })
-  });
+  var events = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    freeMode: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+});
