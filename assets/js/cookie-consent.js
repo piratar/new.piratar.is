93931bc6f9980056f3c66ba016b2cfc9e6c235f3
Gauti Nils Bernhardsson <gauti@reontech.com>
2019-07-22 13:06:19 +0000

    Cookie consent

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,30 @@
+function createCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+$(document).ready(function () {
+  if (!readCookie('cookie-policy-approved')) {
+    $('#cookie-policy').show();
+  }
+
+  $('#cookie-policy-approve').click(function () {
+    createCookie('cookie-policy-approved', 'true', 28);
+    $('#cookie-policy').hide();
+  });
+});
