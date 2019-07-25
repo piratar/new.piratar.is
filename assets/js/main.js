6eda63aa7849c3ba1e5c3153c448bde1b126e37c
Gauti Nils Bernhardsson <gauti@reontech.com>
2019-07-25 17:52:28 +0000

    Search bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,4 @@
 
 {% include_relative event-carousel.js %}
 {% include_relative cta-section.js %}
+{% include_relative search.js %}
---
+++ search.md
@@ -0,0 +1,6 @@
+---
+title: Leit
+layout: search
+lang: is
+permalink: leit
+---
