df9f627a3aab42352fdefb7ff86f2b5841325bad
Gauti Nils Bernhardsson <gauti@reontech.com>
2019-07-26 12:29:36 +0000

    Search css

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,31 +3,43 @@ $(document).ready(function () {
   $('#search-form').submit(function (event) {
     event.preventDefault();
     let string = $("#search-box").val();
-    onSearchPage();
-    if (onSearchPage()) {
+    let searchUrl = $("#search-box").data("searchurl");
+    if (window.location.pathname === searchUrl) {
+      // on the search page
+      updateQuery(string);
       displayResults(fuse.search(string));
-      // TODO change query string
     } else {
-      window.location = '/leit?q=' + string; 
+      // move to search page
+      window.location = searchUrl + '?q=' + string;
     }
   });
 });
 
 $(document).ready(function () {
   if ($('#search-results').length) {
+    $("#search-box").addClass('active');
     const urlParams = new URLSearchParams(window.location.search);
     const string = urlParams.get('q');
-    $.getJSON("{{'/search_data.json' | absolute_url}}", function(list) {
+    $("#search-box").val(string);
+    $.getJSON("{{'/search_data.json' | absolute_url}}", function (list) {
       fuse = new Fuse(list, fuseOptions);
-      displayResults(fuse.search(string));
+      if (string) {
+        displayResults(fuse.search(string));
+      }
     });
   }
 });
 
-const onSearchPage = () => {
-  let path = window.location.pathname.split('/').pop();
-  return path === 'leit' || path === 'search';
-}  
+/**
+ * Updates the query parameter
+ * @param {String} query new query string
+ */
+const updateQuery = (query) => {
+  if (history.pushState) {
+    var url = window.location.protocol + "//" + window.location.host + window.location.pathname + '?q=' + query;
+    window.history.pushState({ path: url }, '', url);
+  }
+}
 
 const fuseOptions = {
   shouldSort: true,
@@ -42,18 +54,21 @@ const fuseOptions = {
   ]
 };
 
-
 const displayResults = (results) => {
-  console.log(results);
-  var $search_results = $("#search-results");
-  $search_results.empty(); // Clear any old results
+  var $searchResults = $("#search-results");
+  var $noResults = $("#no-results");
+  $searchResults.empty(); // Clear any old results
   if (results.length) {
+    $noResults.addClass('d-none');
     results.forEach(result => {
-      var appendString = '<li><a href="' + result.url + '">' + result.title + '</a></li>';
-      $search_results.append(appendString);
+      var appendString = '<a class="result" href="' + result.url + '"><p class="result-title">'
+        + result.title + '</p><p class="result-description">'
+        + trimString(result.content, 130) + '</p></a>';
+      $searchResults.append(appendString);
     });
   } else {
-    // TODO no results
+    $noResults.removeClass('d-none');
   }
 }
 
+const trimString = (string, length) => string.length > length ? string.substring(0, length) + '...' : string;
