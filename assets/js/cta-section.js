e18b8b7ec6ddca0d2a267fb0d29e407ac127cf79
Gauti Nils Bernhardsson <gauti@reontech.com>
2019-09-06 15:06:47 +0000

    Configurable cta buttons

--- assets/js/cta-section.js
+++ assets/js/cta-section.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   var $cta = $('section.cta-random');
+  const ctaButtons = {{ site.data.cta-buttons | jsonify }};
   if ($cta.length) {
-    const ctas = ['cta-register', 'cta-newsletter', 'cta-donate', 'cta-association', 'cta-vote']
+    const ctas = ctaButtons[document.documentElement.lang].map(c => c.template)
       .map((a) => ({ sort: Math.random(), value: a }))
       .sort((a, b) => a.sort - b.sort)
       .map((a) => a.value)
