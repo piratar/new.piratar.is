0b5e7bb25abb5713eafc765f368a586e84a8b1ed
Gauti Nils Bernhardsson <gauti@reontech.com>
2019-09-12 18:20:44 +0000

    Autoplay videos

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -5,11 +5,30 @@ $(document).ready(function () {
 });
 
 function setupHeroCarousel() {
-  return new Swiper('.hero .swiper-container', {
+  const playVideo = (swiper) => {
+    let slides = $('.hero .swiper-slide');
+    slides.find('video').trigger('pause') // pause all videos
+    let slide = slides.get(swiper.activeIndex);
+    if (slide.classList.contains('video')) {
+      let video = slide.children[0];
+      video.muted = true; // mute video
+      video.play(); // play video
+    }
+  }
+  let swiper = new Swiper('.hero .swiper-container', {
+    init: false,
     slidesPerView: 1,
-    loop: true,
+    loop: {{ site.data.hero.loop }},
     autoplay: {
-      delay: 75000,
+      delay: {{ site.data.hero.delay }},
     },
   });
+  swiper.on('slideChange', function() {
+    playVideo(swiper);
+  });
+  swiper.on('init', function() {
+    playVideo(swiper);
+  });
+
+  swiper.init();
 }
