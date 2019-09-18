f30c1cfb6e7f65fbed913e394e8ab044f3429799
Gauti Nils Bernhardsson <gauti@reontech.com>
2019-09-18 15:46:24 +0000

    Hero video use browser controls

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,18 +1,6 @@
 $(document).ready(function () {
   if ($("section.hero .swiper-slide").length > 1) {
     setupHeroCarousel();
-
-    $('section.hero video').click(function() {
-      if (this.paused) {
-        this.play();
-        this.controls = true;
-        $(this).siblings().find('.play-button').fadeOut();
-      }
-    });
-    $('section.hero video').on('pause', function() {
-      this.controls = false;
-      $(this).siblings().find('.play-button').fadeIn();
-    })
   }
 });
 
